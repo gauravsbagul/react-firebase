537f709f2269c4a30db195a71778d027730f5401
Gaurav Sunil Bagul <gauravbagul@winjit.com>
2020-02-05 11:06:09 +0530

    hosted on firebase

---
+++ dist/precache-manifest.a33928b109e1c91d550e8a75e479804c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9229a108742b772540d927a4e5aca928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b8d5e8415d7506d569",
+    "url": "/static/css/main.085565e2.chunk.css"
+  },
+  {
+    "revision": "483798979df0e11f5977",
+    "url": "/static/js/2.519488ff.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/static/js/2.519488ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b8d5e8415d7506d569",
+    "url": "/static/js/main.67c912e0.chunk.js"
+  },
+  {
+    "revision": "c617cfcc9664c8f58169",
+    "url": "/static/js/runtime-main.683d4e5b.js"
+  }
+]);
